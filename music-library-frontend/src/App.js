dd508de2d1c9726160b1b1ec8db1d48dcc1cbeb4
Charles Taylor <charles4312@comcast.net>
2022-11-02 21:36:16 -0400

    Added Basic Axios Communication To BackEnd Application To Be Used In Song Table

--- music-library-frontend/src/App.js
+++ music-library-frontend/src/App.js
@@ -1,7 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import Logo from "./components/Logo/Logo";
-import "./App.css"
+import "./App.css";
+import axios from 'axios';
+
 
 function App() {
+
+  const [songs, setSongs] = useState([])
+
+  useEffect(() => {
+    getAllSongs();
+  }, []);
+
+  async function getAllSongs() {
+    const response = await axios.get('http://127.0.0.1:8000/music/');
+    console.log(response.data);
+    setSongs(response.data);
+  }
+
+  // function addNewSong(song) {
+    
+  //   let tempSongs = [...songs, song];
+    
+  //   setSongs(tempSongs)
+
+  // }
+
   return (
     <div>
       <Logo />
